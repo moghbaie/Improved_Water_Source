4c44a29c4ec11fb6fb4ef235c3b6d676734b097a
moghbaie <moghbaie@stevens.edu>
2016-10-23 19:27:57 -0400

    4th commit

--- Web/app.py
+++ Web/app.py
@@ -16,7 +16,15 @@ def hello():
 
 @app.route('/plot')
 def plot():
-	return render_template('echo.html')
+	countries = ["CL","UY","HU"] 
+	indicators = {'SH.H2O.SAFE.ZS':'Improved water source'}
+	df = wbdata.get_dataframe(indicators, country=countries, convert_date=False)
+	dfu = df.unstack(level=0)
+
+	plotPng = dfu.plot(); 
+	plt.legend(loc='best'); 
+	plt.title("Improved water source (% of population)"); 
+	plt.xlabel('Date'); plt.ylabel('Improved water source (% of population');
 # if __name__ == '__main__':
 #     # Use this port=33507 when you want to Flask to work on Heroku....
 #     app.run()
