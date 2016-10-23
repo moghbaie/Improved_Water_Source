5bbbde4d98d53bca0c6e5e771e405bd5c37eb854
moghbaie <moghbaie@stevens.edu>
2016-10-23 19:00:53 -0400

    third commit

--- Web/app.py
+++ Web/app.py
@@ -6,10 +6,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Hello in from Anaconda'
-
+@app.route("/")
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     # Use this port=33507 when you want to Flask to work on Heroku....
